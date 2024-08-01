07c2db494545ff4fae4d0ff177178566ebfa6871
Prakratiii <71223817+Prakratiii@users.noreply.github.com>
2024-08-01 12:41:32 +0530

    day9 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,26 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String imageUrl;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.imageUrl});
+}
+
+final products = [
+  Item(
+      id: "id",
+      name: "name",
+      desc: "desc",
+      price: 1,
+      color: "color",
+      imageUrl: "imageUrl")
+];
